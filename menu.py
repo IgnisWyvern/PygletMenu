26fc6a75306f992ce09918c9331ea19cb4aab18b
Keian Ho <keian.ho@gmail.com>
2020-02-15 07:36:54 +0000

    Added scaling instead of absolute

--- menu.py
+++ menu.py
@@ -5,6 +5,7 @@ from typing import Callable, Tuple, Dict
 class Menu:
     """
     A collection of buttons and text labels
+    that takes place in a 100x100 that scales with the screen
     """
     def __init__(self, x: int, y: int, width: int, height: int,
                  colour_tuple: Tuple[int, int, int] = (0, 0, 0)):
@@ -12,8 +13,11 @@ class Menu:
         self.y = y
         self.width = width
         self.height = height
+        self.multiplier = (width//100, height//100)
+        self.offset = (((width % 100)//2), ((height % 100)//2))
 
         self.batch = pyglet.graphics.Batch()
+        self.user_fore = pyglet.graphics.OrderedGroup(3)
         self.fore = pyglet.graphics.OrderedGroup(2)
         self.mid = pyglet.graphics.OrderedGroup(1)
         self.back = pyglet.graphics.OrderedGroup(0)
@@ -35,14 +39,12 @@ class Menu:
         if name not in self.button_dict:
             self.button_dict[name] = self.Button(text, x, y, width,
                                                  height, on_click,
-                                                 colour_tuple=colour_tuple,
-                                                 fore_group=self.fore,
-                                                 mid_group=self.mid,
-                                                 batch=self.batch,
-                                                 font_name=font_name,
-                                                 font_size=font_size)
+                                                 colour_tuple, self.batch,
+                                                 self.fore, self.mid,
+                                                 self.multiplier, self.offset,
+                                                 font_name, font_size)
             return self.button_dict[name]
-        return None
+        raise Exception("A button of that name already exists")
 
     def add_texttag(self, name: str, text: str, x: int, y: int, width: int,
                     height: int,
@@ -53,10 +55,11 @@ class Menu:
             self.texttag_dict[name] = self.TextTag(text, x, y, width,
                                                    height, colour_tuple,
                                                    self.batch, self.fore,
-                                                   self.mid, font_name,
+                                                   self.mid, self.multiplier,
+                                                   self.offset, font_name,
                                                    font_size)
             return self.texttag_dict[name]
-        return None
+        raise Exception("A texttag of that name already exits")
 
     def draw(self):
         """draw all the elements of the menu"""
@@ -66,13 +69,27 @@ class Menu:
         """
         Check all the buttons for if the cursor was in their domain
         """
+        mul_x, mul_y = self.multiplier
+        off_x, off_y = self.offset
+        x -= off_x
+        x /= mul_x
+        y -= off_y
+        y /= mul_y
         for button in self.button_dict.values():
             button.check_click(x, y)
+
+    def resize_contents(self):
+        for button in self.button_dict.values():
+            button.resize(self.multiplier)
+
+        for texttag in self.texttag_dict.values():
+            texttag.resize(self.multiplier)
 
     class TextTag:
         def __init__(self, text: str, x: int, y: int, width: int, height: int,
                      colour_tuple: Tuple[int, int, int], batch, fore_group,
-                     mid_group, font_name: str, font_size: int):
+                     mid_group, multiplier, offset, font_name: str,
+                     font_size: int):
             self.text = text
             self.x = x
             self.y = y
@@ -82,24 +99,27 @@ class Menu:
             self.font_name = font_name
             self.batch = batch
 
-            if isinstance(batch, pyglet.graphics.Batch):
-                self.vertex_list = batch.add(4, pyglet.gl.GL_QUADS, mid_group,
-                                             ("v2i", (x, y, x + width, y, x +
-                                              width, y + height, x,
-                                              y + height)), 
-                                             ("c3B", (colour_tuple * 4)))
-            else:
-                self.vertex_list = pyglet.graphics.vertex_list(4,
-                                            ("v2i", (x, y, x + width, y, x +
-                                                     width, y + height,
-                                                     x, y + height)),
-                                            ("c3B", (colour_tuple * 4)))
+            mul_x, mul_y = multiplier
+            off_x, off_y = offset
+
+            x *= mul_x
+            x += off_x
+            width *= mul_x
+            y *= mul_y
+            y += off_y
+            height *= mul_y
+
+            self.vertex_list = batch.add(4, pyglet.gl.GL_QUADS, mid_group,
+                                         ("v2i", (x, y, x + width, y, x +
+                                          width, y + height, x,
+                                          y + height)),
+                                         ("c3B", (colour_tuple * 4)))
 
             self.label = pyglet.text.Label(self.text, font_name=self.font_name,
                                            font_size=self.font_size,
                                            group=fore_group,
-                                           x=self.x + self.width/2,
-                                           y=self.y + self.height/2,
+                                           x=x + width/2,
+                                           y=y + height/2,
                                            anchor_x="center",
                                            anchor_y="center",
                                            batch=self.batch)
@@ -110,13 +130,19 @@ class Menu:
             self.vertex_list.draw(pyglet.gl.GL_QUADS)
             self.label.draw()
 
+        def resize(self, multiplier, offset):
+            pass
+
     class Button(TextTag):
         def __init__(self, text: str, x: int, y: int, width: int, height: int,
                      on_click: Callable,
                      colour_tuple: Tuple[int, int, int], batch, fore_group,
-                     mid_group, font_name: str, font_size: int):
+                     mid_group, multiplier, offset, font_name: str,
+                     font_size: int):
+
             super().__init__(text, x, y, width, height, colour_tuple, batch,
-                             fore_group, mid_group, font_name, font_size)
+                             fore_group, mid_group, multiplier, offset,
+                             font_name, font_size)
             self.on_click = on_click
 
         def check_click(self, cur_x: int, cur_y: int):
@@ -126,11 +152,11 @@ class Menu:
 
 
 def main():
-    window = pyglet.window.Window()
-    menu = Menu(window.width, window.height)
-    menu.add_button("he", "hi", 0, 0, 200, 200, lambda: print("hi"))
-    menu.add_button("hi", "bye", 200, 200, 200, 200, lambda: print("bye"))
-    menu.add_texttag("hl", "la", 400, 400, 100, 100)
+    window = pyglet.window.Window(resizable=True)
+    menu = Menu(0, 0, window.width, window.height)
+    menu.add_button("he", "hi", 0, 0, 25, 25, lambda: print("hi"))
+    menu.add_button("hi", "bye", 25, 25, 25, 45, lambda: print("bye"))
+    menu.add_texttag("hl", "la", 50, 70, 50, 30)
     @window.event
     def on_mouse_press(x, y, button, modifiers):
         if button == pyglet.window.mouse.LEFT:
